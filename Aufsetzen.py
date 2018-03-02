1a4174c1e1f4d5bb18310320b82bd3433d4e37de
TheGameBros <niklas-schwarz@online.de>
2018-03-02 22:55:25 +0100

    Add files via upload

---
+++ Aufsetzen.py
@@ -0,0 +1,39 @@
+import sqlite3
+connection = sqlite3.connect("Arbeitszeiten.db")
+c = connection.cursor()
+
+sql_command = """
+CREATE TABLE Arbeiter1 ( 
+Tag, 
+Datum DATE, 
+Ankunftszeit,
+Abfahrtszeit,
+Zeitstempel,
+Arbeitszeit);"""
+
+c.execute(sql_command)
+
+sql_command = """
+CREATE TABLE Arbeiter2 ( 
+Tag, 
+Datum DATE, 
+Ankunftszeit, 
+Abfahrtszeit,
+Zeitstempel,
+Arbeitszeit);"""
+
+c.execute(sql_command)
+
+sql_command = """
+    CREATE TABLE Arbeiter3 ( 
+    Tag, 
+    Datum DATE, 
+    Ankunftszeit, 
+    Abfahrtszeit,
+    Zeitstempel,
+    Arbeitszeit);"""
+
+c.execute(sql_command)
+
+connection.commit()
+connection.close()
